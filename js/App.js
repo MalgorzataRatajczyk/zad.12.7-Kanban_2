6bf222db3bed7c8479e666e85e799fc9c0af6da7
Malgorzata Ratajczyk <malgorzataratajczyk@wp.pl>
2018-09-26 20:57:08 +0200

    Add App.js, Board.js, Card.js and Column.js

---
+++ js/App.js
@@ -0,0 +1,62 @@
+
+// zmienne, z których będziemy korzystać przy komunikacji z serwerem:
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+    'X-Client-Id': '3526',
+    'X-Auth-Token': '6df3383a5b825242bbae60bf0001d132'
+};
+
+function generateTemplate(name, data, basicElement) {
+
+  	var template = document.getElementById(name).innerHTML;
+  	var element = document.createElement(basicElement || 'div');
+  
+        Mustache.parse(template);
+      
+  	element.innerHTML = Mustache.render(template, data);
+  
+  	return element;
+}
+
+//funkcję odpytującą serwer o zasób tablicy. 
+fetch(baseUrl + '/board', { headers: myHeaders })
+
+    .then(function(resp) {
+
+    return resp.json();
+
+    })
+
+    .then(function(resp) {
+
+setupColumns(resp.columns);
+
+});
+  
+  //implementacji funkcji setupColumns()
+function setupColumns(columns) {
+
+    columns.forEach(function(column) {
+
+        var col = new Column(column.id, column.name);
+        
+    board.addColumn(col);
+
+    setupCards(col, column.cards); //tworzenie kart
+
+    });
+
+}
+//implementacja funkcji setupCards()
+function setupCards(col, cards) {
+
+	cards.forEach(function (card) {
+
+        var cardObj = new Card(card.id, card.name);
+        
+  	col.addCard(cardObj);
+  	
+    });
+    
+}
+  
